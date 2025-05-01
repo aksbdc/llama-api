1caf73047151649ce95d5b4b655edff9f0b487e2
Solomon Himelbloom <7608183+TechSolomon@users.noreply.github.com>
2025-04-30 16:51:05 -0800

    feat: entrypoint for REST API playground

--- main.py
+++ main.py
@@ -2,15 +2,16 @@ import os
 from llama_api_client import LlamaAPIClient
 
 client = LlamaAPIClient(
-    api_key=os.environ.get("LLAMA_API_KEY"),
-    base_url="https://api.llama.com/",
+    api_key=os.environ.get("LLAMA_API_KEY"),  # This is the default and can be omitted
 )
 
-response = client.chat.completions.create(
-    model="Llama-4-Maverick-17B-128E-Instruct-FP8",
+create_chat_completion_response = client.chat.completions.create(
     messages=[
-        {"role": "user", "content": "What is the AI Resource Program @ Alaska SBDC?"},
+        {
+            "content": "string",
+            "role": "user",
+        }
     ],
+    model="Llama-4-Maverick-17B-128E-Instruct-FP8",
 )
-
-print(response)
+print(create_chat_completion_response.completion_message)
